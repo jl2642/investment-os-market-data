3020a220ccca1f6742cf466c7be54de2b7ef8c56
jl2642 <ljjx2020@gmail.com>
2026-07-19 11:04:26 +0800

    FMDL-3C-B: route profiles through hardened module

--- scripts/fmdl3cb_core.py
+++ scripts/fmdl3cb_core.py
@@ -1,3 +1,4 @@
 from scripts.fmdl3cb_common import *  # noqa: F401,F403
 from scripts.fmdl3cb_formula import *  # noqa: F401,F403
 from scripts.fmdl3cb_inputs import *  # noqa: F401,F403
+from scripts.fmdl3cb_profiles import *  # noqa: F401,F403
